21cd592b8533abe096ccfb4f580c5a5fcae7dd4c
Takeru Ohta <phjgt308@gmail.com>
2012-01-16 07:18:34 +0900

    built-in: null, mapcar, first, second, let

--- cc/data/stdlib.lisp
+++ cc/data/stdlib.lisp
@@ -15,20 +15,37 @@
 (defmacro setq (symbol value)
   (list 'set-symbol-value (list 'quote symbol) value))
 
+;; null
+(defun null (list)
+  (eq list nil))
+
 ;; mapcar 
-#|
 (defun mapcar (fn list)
-  (if (
+  (if (null list)
+      nil
+    (cons (fn (car list))
+          (mapcar fn (cdr list)))))
 
-(defmacro let (
-|#
+;; first
+(defun first (list)
+  (car list))
 
+;; second
+(defun second (list)
+  (car (cdr list)))
+
+;; let
+(defmacro let (binds &rest body)
+  (cons (cons 'lambda (cons (mapcar first binds) body))
+        (mapcar second binds)))
+
+(mapcar (lambda (n) (+ n n 1)) '(1 2 3))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun fib (n)
   (if (< n 2)
       n
     (+ (fib (- n 2)) (fib (- n 1)))))
 
-(list (fib 21)
-      (fib 22))
-
-a
+(let ((n 10))
+  (list (fib n) (fib (+ n 1))))
