7e147c559246c6df6c543bad19e64c9070af7dc9
Takeru Ohta <phjgt308@gmail.com>
2012-01-26 12:34:24 +0900

    サンプル追加

--- cc/vm/sample.lisp
+++ cc/vm/sample.lisp
@@ -1,16 +1,22 @@
 (load "pvm-compile")
 
+;; addition
+(pvmc:compile-to-file
+ "/tmp/add.bc"
+ '(10 20 :add))  ; 10 + 20
+
+;; Fibonacci number
 (pvmc:compile-to-file
  "/tmp/fib.bc"
  '(
-   25
-   (:addr fib-beg) :call
+   25 ; argument
+   (:addr fib-beg) :call ; fib(25)
    (:addr finish)  :jump
    
    fib-beg
-   :dup 2  :less (:addr fib-end) :jump-if
-   :dup 2  :sub  (:addr fib-beg) :call
-   :swap 1 :sub  (:addr fib-beg) :call
+   :dup 2  :less (:addr fib-end) :jump-if  ; if(n < 2) 
+   :dup 2  :sub  (:addr fib-beg) :call     ; fib(n - 2)
+   :swap 1 :sub  (:addr fib-beg) :call     ; fib(n - 1)
    :add
    fib-end
    :return
