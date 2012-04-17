9cf8bf7764bd01d713ca2c074d66e8438885f673
Takeru Ohta <phjgt308@gmail.com>
2012-04-17 18:55:21 +0900

    ファイル操作系の基礎関数追加

--- vm/executor/native-function.lisp
+++ vm/executor/native-function.lisp
@@ -26,6 +26,24 @@
 
 (defnative $load-bytecode-file (path) (load-bc path))
 
+(defmacro unix-call (name &rest args)
+  `(multiple-value-bind (fd err) (,(find-symbol (symbol-name name) :sb-unix) ,@args)
+     (when err
+       (format *error-output* "~&; ~a) err[~a]: ~a~%" ',name err (sb-int:strerror err)))
+     fd))
+
+(defnative $open (path flags mode) 
+  (let ((fd (unix-call unix-open path flags mode)))
+    (and fd (sb-sys:make-fd-stream fd :element-type 'octet :input t :output t))))
+
+(defnative $close (stream) (close stream))
+(defnative $read-byte (stream) (read-byte stream))
+(defnative $write-byte (stream byte) (write-byte stream byte))
+(defnative $read-chunk (stream buffer start length) 
+  (read-sequence buffer stream :start start :end (+ start length)))
+(defnative $write-chunk (stream buffer start length)
+  (write-sequence buffer stream :start start :end (+ start length)))
+
 (defparameter *natives*
   `(
     (:$+ ,$add)
@@ -44,6 +62,17 @@
     (:$cdr ,$cdr)
 
     (:$load-bytecode-file ,$load-bytecode-file)
+
+    (:$open ,$open)
+    (:$close ,$close)
+    (:$read-byte ,$read-byte)
+    (:$write-byte ,$write-byte)
+    (:$read-chunk ,$read-chunk)
+    (:$write-chunk ,$write-chunk)
+    (:$O_RDONLY ,sb-unix:o_rdonly)
+    (:$O_WRONLY ,sb-unix:o_wronly)
+    (:$O_RDWR ,sb-unix:O_RDWR)
+    (:$O_CREAT ,sb-unix:O_CREAT)
     ))
 
 ;;;;;
