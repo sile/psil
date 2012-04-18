1766a192f7fa64446477467f6de06c05a1cef701
Takeru Ohta <phjgt308@gmail.com>
2012-04-18 12:17:38 +0900

    ごく基本的な入力関数を定義するバイトコード生成関数作成

--- core/pbc-core.lisp
+++ core/pbc-core.lisp
@@ -5,7 +5,25 @@
 ;; - parse
 ;; - compile
 
-(defun gen-parse-bc ()
+(defun gen-basic-io-bc ()
+  (compile-bytecode
+   '(progn 
+      (defun open-input-file (filename)
+        ($open filename $o_rdonly #o666))
 
-  )
+      (defun close-input-port (port)
+        ($close port))
+      
+      (defun read-char (port)
+        ($code-char ($read-byte port)))
+
+      (defun eof? (port)
+        ($eof-p port))
+      )))
+
+
+;; for dev
+(defun w (name bc)
+  (pvm:write-bytecodes-to-file (format nil "/tmp/~a.bc" name) bc))
+
 
