84f7bbae426edd824db4a867760793563aa8a14e
Takeru Ohta <phjgt308@gmail.com>
2012-04-22 04:57:41 +0900

    データスタック/リターンスタック実装

--- plvm/include/psil/vm/DataStack.hh
+++ plvm/include/psil/vm/DataStack.hh
@@ -1,10 +1,46 @@
 #ifndef PSIL_VM_DATA_STACK_HH
 #define PSIL_VM_DATA_STACK_HH
 
+#include "Object.hh"
+#include <vector>
+
+/*
+  [stack-layout]
+  -- top --
+  x2
+  x1
+  local-var2
+  local-var1
+  -- base --
+  arg1
+  arg2
+
+  [in heap]
+  closed-var1
+  closed-var2
+ */
 namespace psil {
   namespace vm {
     class DataStack {
+    public:
+      DataStack() : base(0) {} 
       
+      void push(type::Object* o) { stack.push_back(o); }
+      type::Object* pop() { type::Object* v=stack.front(); stack.pop_back(); return v; }
+      void reserve(unsigned count) { stack.resize(stack.size()+count); }
+      void drop(unsigned count) { stack.resize(stack.size()-count); }
+      void reset(unsigned base, unsigned top) { 
+        this->base = base;
+        stack.resize(top);
+      }
+      
+      type::Object* arg_get(unsigned index) { return stack[base-index-1]; }
+      void arg_set(unsigned index, type::Object* value) { stack[base-index-1] = value; }
+      type::Object* local_get(unsigned index) { return stack[base+index]; }
+      void local_set(unsigned index, type::Object* value) { stack[base+index] = value; }
+    private:
+      unsigned base;
+      std::vector<type::Object*> stack;
     };
   }
 }
--- plvm/include/psil/vm/ReturnStack.hh
+++ plvm/include/psil/vm/ReturnStack.hh
@@ -1,10 +1,39 @@
 #ifndef PSIL_VM_RETURN_STACK_HH
 #define PSIL_VM_RETURN_STACK_HH
 
+#include "Object.hh"
+#include <vector>
+
+/*
+  [layout]
+  previous-top
+  previous-base
+  return-address
+*/
 namespace psil {
   namespace vm {
     class ReturnStack {
+    public:
+      struct Entry {
+        Entry(unsigned t, unsigned b, unsigned r) : top(t), base(b), returnAddress(r) {}
+        unsigned top;
+        unsigned base;
+        unsigned returnAddress;
+      };
+
+    public:
+      void push(const Entry& e) {
+        stack.push_back(e);
+      }
       
+      Entry pop() {
+        const Entry& e = stack.front();
+        stack.pop_back();
+        return e;
+      }
+      
+    private:
+      std::vector<Entry> stack;
     };
   }
 }
