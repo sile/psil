f32ba094543b7b87fd88ba23112dd7ff8440b21a
Takeru Ohta <phjgt308@gmail.com>
2012-05-05 09:49:49 +0900

    複数ファイル同時実行時のバグ修正

--- plc/stdlib/io.lisp
+++ plc/stdlib/io.lisp
@@ -1,10 +1,9 @@
 ;(begin
 (begin
  (define fnfn (lambda (fn)
-                show-stack
                 20
                 fn))
- ;(fnfn 10)
+ (fnfn 10)
  )
 
 ; (define call-with-input-file (lambda (filepath proc)
--- plvm/src/bin/plvm.cc
+++ plvm/src/bin/plvm.cc
@@ -22,14 +22,14 @@ int main(int argc, char** argv) {
     const char* filepath = argv[i];
     std::cout << "# " << filepath << std::endl;
     
-    vm::FileContent fc(filepath);
+    vm::FileContent& fc = * new vm::FileContent(filepath);
     if(! fc) {
       std::cerr << "ERROR: can't open file" << std::endl;
       return 1;
     }
     
-    vm::ByteStream bs(fc.bytes(), fc.size());
-    vm::BytecodeObject bo(bs);
+    vm::ByteStream& bs = * new vm::ByteStream(fc.bytes(), fc.size());
+    vm::BytecodeObject& bo = * new vm::BytecodeObject(bs);
     if(! bo) {
       std::cerr << "ERROR: can't parse " << filepath << std::endl;
       return 1;
