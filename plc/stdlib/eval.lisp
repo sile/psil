a30aed122ae1d87b5d7df6312cbc946cfc10cd4d
Takeru Ohta <phjgt308@gmail.com>
2012-05-07 00:51:46 +0900

    save

--- plc/stdlib/eval.lisp
+++ plc/stdlib/eval.lisp
@@ -1,4 +1,11 @@
 (begin
- (define eval (lambda (exp)
+ (define compile (lambda (exp)
    ))
+
+ (define eval-bytecode (lambda (bytecode)
+   ))
+
+ (define eval (lambda (exp . environment-specifier)
+   (let ((bytecode (compile exp)))
+     (eval-bytecode bytecode))))
  )
